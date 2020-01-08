a861f6fa31f20c4d786a2f90f7907a298590efa5
bellweerin <weerin.n@ku.th>
2020-01-08 20:54:12 +0700

    edit random page + edit csszen

---
+++ public/js/random.js
@@ -0,0 +1,82 @@
+var i = 0;
+var nameList = [];
+var giftList = [];
+var randomGift = [];
+
+function Input() {
+    let name = document.getElementById("nameInput").value;
+    let gift = document.getElementById("giftInput").value;
+    console.log(name);
+    console.log(gift);
+    document.getElementById("nameInput").value = '';
+    document.getElementById("giftInput").value = '';
+    if (name != '') {
+        nameList[i] = name;
+        giftList[i] = gift;
+        i++;
+
+        var tbody = document.getElementById("tbody");
+        var tr = document.createElement("tr");
+
+        var tdName = document.createElement("td");
+        var name1 = document.createTextNode(name);
+        tdName.appendChild(name1);
+
+        var tdGift = document.createElement("td");
+        var gift1 = document.createTextNode(gift);
+        tdGift.appendChild(gift1);
+
+        tr.appendChild(tdName);
+        tr.appendChild(tdGift);
+
+        tbody.appendChild(tr);
+    }
+    console.log(i);
+    console.log(nameList);
+    console.log(giftList);
+
+}
+
+function random() {
+    do {
+        let r = giftList[Math.floor(Math.random() * giftList.length)];
+        randomGift.push(r);
+        randomGift = randomGift.filter((item, index) => {
+            return randomGift.indexOf(item) === index
+        });
+    } while (randomGift.length < giftList.length);
+
+    for(let i=0;i<randomGift.length;++i){
+        console.log(nameList[i]);
+        console.log(randomGift[i]);
+        var tbody = document.getElementById("tbody2");
+        var tr = document.createElement("tr");
+
+        var tdName = document.createElement("td");
+        var name2 = document.createTextNode(nameList[i]);
+        tdName.appendChild(name2);
+
+        var tdGift = document.createElement("td");
+        var gift2 = document.createTextNode(randomGift[i]);
+        tdGift.appendChild(gift2);
+
+        tr.appendChild(tdName);
+        tr.appendChild(tdGift);
+
+        tbody.appendChild(tr);
+
+    }
+    while(randomGift.length>0){
+        randomGift.pop();
+    }
+
+    console.log(randomGift);
+
+
+
+}
+
+
+
+
+
